29ca6a352df05835c50333a945a2f02042d4de99
WoahItsJeebus <147510817+WoahItsJeebus@users.noreply.github.com>
2026-03-05 16:22:19 -0600

    Icon fix

--- assets/site.js
+++ assets/site.js
@@ -2,7 +2,7 @@
    site.js — shared helpers for all pages
    ============================================================ */
    
-   export const _VERSION = "1.0.0.3";
+   export const _VERSION = "1.0.0.4";
    
 /**
  *
