2489f5133300e55b3a52ddb7669a39956e2ec49a
WoahItsJeebus <147510817+WoahItsJeebus@users.noreply.github.com>
2026-03-05 18:26:32 -0600

    1.0.0.5a

--- assets/site.js
+++ assets/site.js
@@ -2,7 +2,7 @@
    site.js — shared helpers for all pages
    ============================================================ */
    
-   export const _VERSION = "1.0.0.5";
+   export const _VERSION = "1.0.0.5a";
    
 /**
  *
